54e5ad50397973854f7751375bd4bd86a184866f
canary4651 <canary4651@gmail.com>
2021-04-19 01:30:19 +0900

    Apply scroll to top
    Apply scroll to top on the record page.

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,3 +3,7 @@ export const get = (key) => (state) => state[key];
 export function equal(key, value) {
   return (obj) => obj[key] === value;
 }
+
+export function scrollToTop() {
+  window.scrollTo(0, 0);
+}
